de6353893b48063c2a00d3bd675eb5a7049784a6
Gabriel Sousa <gabrielsou1912@gmail.com>
2023-10-18 17:43:06 -0300

    new feat

--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,23 +1,52 @@
 <template>
     <div class="todoListContainer">
         <div class="heading">
-            <h2 id="tittle">To Do List</h2>
+            <h2 id="title">To Do List</h2>
             <add-item-form />
+                v-on:reloadList="getList()"
+                />
         </div>
-        <list-view />
+        <list-view
+            :items="items"
+             v-on:reloadList="getList()"/>
+
     </div>
 </template>
 
 <script>
 import addItemForm from './addItemForm.vue';
 import listView from './listView.vue';
+
 export default{
     components: {
         addItemForm,
         listView
+    },
+
+    data: function () {
+        return {
+            items: []
+        }
+    },
+    methods: {
+        getList () {
+            axios.get('api/items')
+            .then(response => {
+                    this.item = response.data
+            })
+            .catch(error =>{
+                console.log(error);
+            })
+        }
+    },
+
+    created(){
+        this.getList();
+
     }
 }
 </script>
+</html>
 
 <style scoped>
 .todoListContainer{
@@ -28,9 +57,7 @@ export default{
 .heading{
     background: #e6e6e6;
     padding: 10px;
-}
-
-#title{
+} #title{
     text-align: center;
     margin: auto;
 }
